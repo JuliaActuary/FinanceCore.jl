5e6537c5ee900300454f5b00565638ed7e5016ad
Alec Loudenback <alecloudenback@gmail.com>
2023-07-04 13:57:21 -0500

    API to handle CFs and real/times

--- src/pv.jl
+++ src/pv.jl
@@ -1,39 +1,29 @@
 """
-    present_value(interest, cashflows::Vector, timepoints)
-    present_value(interest, cashflows::Vector)
+    present_value(yield_model, cashflows[, timepoints=pairs(cashflows)])
 
-Discount the `cashflows` vector at the given `interest_interestrate`,  with the cashflows occurring
-at the times specified in `timepoints`. If no `timepoints` given, assumes that cashflows happen at times 1,2,...,n.
+Discount the `cashflows` vector at the given `yield_model`,  with the cashflows occurring
+at the times specified in `timepoints`. If no `timepoints` given, assumes that cashflows happen at the indices of the cashflows.
 
-The `interest` can be an `InterestCurve`, a single scalar, or a vector wrapped in an `InterestCurve`. 
+If your timepoints are dates, you can convert them into a floating point representation of the time interval using DayCounts.jl.
 
 # Examples
 ```julia-repl
 julia> present_value(0.1, [10,20],[0,1])
 28.18181818181818
-julia> present_value(Yields.Forward([0.1,0.2]), [10,20],[0,1])
-28.18181818181818 # same as above, because first cashflow is at time zero
-```
-
-Example on how to use real dates using the [DayCounts.jl](https://github.com/JuliaFinance/DayCounts.jl) package
-```jldoctest
-
-using DayCounts 
-dates = Date(2012,12,31):Year(1):Date(2013,12,31)
-times = map(d -> yearfrac(dates[1], d, DayCounts.Actual365Fixed()),dates) # [0.0,1.0]
-present_value(0.1, [10,20],times)
-
-# output
-28.18181818181818
-
+julia> present_value(Continuous(0.1), [10,20],[0,1])
+28.096748360719193
+julia> present_value(Continuous(0.1), [10,20],[1,2])
+25.422989241919232
+julia> present_value(Continuous(0.1), [10,20])
+25.422989241919232
 ```
 
 """
 function present_value(r, x, times)
-    mapreduce(xi, ti -> present_value(r, xi, ti), +, x, times)
+    LoopVectorization.vmapreduce((xi, ti) -> present_value(r, xi, ti), +, x, times)
 end
 function present_value(r, x)
-    mapreduce(px -> present_value(r, last(px), first(px)), +, pairs(x))
+    LoopVectorization.vmapreduce(px -> present_value(r, last(px), first(px)), +, pairs(x))
 end
 
 # time is ignored in favor of the time inside the cashflow
