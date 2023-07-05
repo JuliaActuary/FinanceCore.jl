efc4d66a22ed6d1c159ca3df11b200826a6eba81
Alec Loudenback <alecloudenback@gmail.com>
2023-07-04 21:45:27 -0500

    add `time` and `amount` for downstream algorithms

--- src/Contracts.jl
+++ src/Contracts.jl
@@ -16,8 +16,46 @@ struct Cashflow{N<:Real,T<:Timepoint} <: AbstractContract
     time::T
 end
 
-maturity(c::Cashflow) = c.time
-Base.isapprox(a::Cashflow, b::Cashflow) = isapprox(a.amount, b.amount) && isapprox(a.time, b.time)
+maturity(c::C) where {C<:Cashflow} = c.time
+
+"""
+    amount(x)
+
+If is an object with an amount component (e.g. a `Cashflow`), will retrun that amount component, otherwise just `x`.
+
+# Examples
+
+```julia-repl
+julia> FinanceCore.amount(Cashflow(1.,3.))
+1.0
+
+julia> FinanceCore.amount(1.)
+1.0
+```
+"""
+amount(x::C) where {C<:Cashflow} = x.amount
+amount(x::R) where {R<:Real} = x
+
+"""
+    time(x,t)
+
+If `x` is an object with a defined time component (e.g. a `Cashflow`), will return that time component, otherwise
+will return `t`. This is useful in handling situations where you want to handle either `Cashflow`s or separate amount and time vectors.
+
+# Example
+
+```julia-repl
+julia> FinanceCore.time(Cashflow(1.,3.),"ignored")
+3.0
+
+julia> FinanceCore.time(1.,4.)
+4.0
+```
+
+"""
+time(x::C, t) where {C<:Cashflow} = x.time
+time(x::R, t) where {R<:Real} = t
+Base.isapprox(a::C, b::C) where {C<:Cashflow} = isapprox(a.amount, b.amount) && isapprox(a.time, b.time)
 Base.convert(::Type{Cashflow{A,B}}, y::Cashflow{C,D}) where {A,B,C,D} = Cashflow(A(y.amount), B(y.time))
 
 struct Composite{A,B} <: AbstractContract
